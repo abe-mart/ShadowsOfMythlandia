3e141aa7a9ab3dcbcad749bc82ed5ee30c3f9696
abe-mart <19436449+abe-mart@users.noreply.github.com>
2022-03-05 18:33:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title('Shadows of Mythlandia')
-st.title('The Forge and the Fire')
+st.title('Shadows of Mythlandia: The Forge and the Fire')
 
 st.write('The up and coming best game of 2022. -Everyone Ever')
