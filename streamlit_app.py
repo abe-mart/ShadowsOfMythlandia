d0a120ec9e88a8443e5ee41d197f2fe2efe3bce5
abe-mart <19436449+abe-mart@users.noreply.github.com>
2022-03-05 18:42:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,19 @@ text = ""
 
 b1 = st.button('Attack!')
 
+b2 = st.button('Snow?')
+
+b3 = st.button('Ballons!')
+
 if b1:
   text = "Ow, what was that for?"
+  
+if b2:
+  text = 'Let it snow, let it snow'
+  st.snow()
+  
+if b3:
+  text = 'Hooray!'
+  st.balloons()
   
 st.text_area('',value=text,height=10,disabled=True)
