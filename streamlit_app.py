d0518d06fe4a8077368ebe7f143f279321bbc436
abe-mart <19436449+abe-mart@users.noreply.github.com>
2022-03-06 16:54:08 -0700

    Add logo and rearrange buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,34 @@
 import streamlit as st
 
-st.image('Images/Shadows.png')
+st.image('Images/Shadows.png',use_column_width=True)
 
-st.write('The up and coming best game of 2022. -Everyone Ever')
+# %% Persistent variables
 
-text = ""
+# Initialize text for text box
+if 'text' not in st.session_state:
+    st.session_state['text'] = ""
+    
+# %% Process player inputs
+    
+def process_inputs():
+    text = ""
+    if st.session_state.attack:
+        text = "Ow, what was that for?"
+    if st.session_state.ballon:
+        st.balloons()
+        text = "Hooray!"
+    st.session_state['text'] = text
+    
+# %% Main text area for game display
+st.text_area('',value=st.session_state['text'],height=10,disabled=True)
 
+# %% Buttons
 col1, col2 = st.columns([0.1,0.9])
 
 with col1:
-  b1 = st.button('Attack!')
+  st.button('Attack!',key='attack',on_click=process_inputs)
 
 with col2:
-  b2 = st.button('Ballons!')
-
-if b1:
-  text = "Ow, what was that for?"
-  
-if b2:
-  text = 'Hooray!'
-  st.balloons()
-  
-st.text_area('',value=text,height=10,disabled=True)
+  st.button('Ballons!',key='ballon',on_click=process_inputs)
+      
+st.write('The up and coming best game of 2022. -Everyone Ever')
