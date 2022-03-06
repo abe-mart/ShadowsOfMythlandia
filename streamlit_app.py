db67219e76dbb125c5dd7bce42e89b6bd42e7110
abe-mart <19436449+abe-mart@users.noreply.github.com>
2022-03-05 18:38:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ st.title('Shadows of Mythlandia: The Forge and the Fire')
 
 st.write('The up and coming best game of 2022. -Everyone Ever')
 
-text = ''
+text = ""
 
 b1 = st.button('Attack!')
 
 if b1:
-  text = 'Ow, what was that for?'
+  text = "Ow, what was that for?"
   
 st.text_area(value=text,height=10,disabled=True)
